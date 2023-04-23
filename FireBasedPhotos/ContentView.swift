34e907c4a0194f8ec9a884bea3f93cf48e7f87f7
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-04-23 13:18:17 +0200

    Base setup complete, UIkit image picker being added

--- FireBasedPhotos/ContentView.swift
+++ FireBasedPhotos/ContentView.swift
@@ -2,12 +2,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPickerShowing = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                isPickerShowing = true
+            } label: {
+                Text("Select a Photo")
+            }
+            .buttonStyle(.bordered)
+        }
+        .sheet(isPresented: $isPickerShowing){
+            
         }
         .padding()
     }
---
+++ FireBasedPhotos/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  FireBasedPhotos
+//
+//  Created by Pat on 2023/04/23.
+//
+
+import Foundation
