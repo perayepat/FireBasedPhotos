826032a45a418c925e83689b366bb6f965f0bff2
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-04-23 13:02:17 +0200

    Initial Commit

---
+++ FireBasedPhotos/FireBasedPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBasedPhotosApp.swift
+//  FireBasedPhotos
+//
+//  Created by Pat on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBasedPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
