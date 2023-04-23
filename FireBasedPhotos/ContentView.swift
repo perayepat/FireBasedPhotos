f8c3bd9855a6a0fcfc4239209cb6cd50d5f5b3b1
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-04-23 13:44:58 +0200

    Image place holder created to link the UIViewRep to the SwiftUI view

--- FireBasedPhotos/ContentView.swift
+++ FireBasedPhotos/ContentView.swift
@@ -3,9 +3,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isPickerShowing = false
+    @State var selectedImage: UIImage?
     
     var body: some View {
         VStack {
+            selectedImageView
+            
             Button {
                 isPickerShowing = true
             } label: {
@@ -14,9 +17,28 @@ struct ContentView: View {
             .buttonStyle(.bordered)
         }
         .sheet(isPresented: $isPickerShowing){
-            
+            ImagePicker()
         }
         .padding()
+    }
+    
+    var selectedImageView: some View{
+        VStack {
+            if  selectedImage != nil {
+                Image(uiImage: selectedImage!)
+                    .resizable()
+                    .frame(width: 200, height: 200)
+            } else{
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.gray.opacity(0.5))
+                    .overlay {
+                        Text("Please select a Image")
+                            .foregroundColor(.primary)
+                    }
+                
+            }
+        }
     }
 }
 
--- FireBasedPhotos/ImagePicker.swift
+++ FireBasedPhotos/ImagePicker.swift
@@ -1,8 +1,36 @@
-//
-//  ImagePicker.swift
-//  FireBasedPhotos
-//
-//  Created by Pat on 2023/04/23.
-//
 
 import Foundation
+import UIKit
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable{
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+    /// Create a object that can recieve UIImagePickerController picker controller events
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator{
+        /// Accessed when `context.coordinator` is called
+        return Coordinator()
+    }
+}
+
+class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+///  `UINavigationControllerDelegate` - when the navigation controller shows a new top view controller via a push, pop or setting of the view controller stack
+
+    //On user selection
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+
+}
